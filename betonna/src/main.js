089cbbe86ae2f07f9116a48ce1b9d138ecc980e9
Mohamad Hegazy <mhd123heg@gmail.com>
2023-09-10 18:27:13 +0300

    10/9/2023

--- betonna/src/main.js
+++ betonna/src/main.js
@@ -8,14 +8,5 @@ import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 import "popper.js";
 import "aframe";
-import VueSocialSharing from "vue-social-sharing";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(VueSocialSharing, {
-    networks: {
-      fakeblock: "https://fakeblock.com/share?url=@url&title=@title",
-    },
-  })
-  .mount("#app");
+createApp(App).use(store).use(router).mount("#app");
